9dce47718ac60c0affb0c04cbb1a10ed253942b4
Lia Brodeur <brodeur.lia@gmail.com>
2016-11-03 17:38:25 -0400

    updated print statements

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import socket, sys, time    # Import socket module
 
+
+# need to find a way to automatically boot capstone server code 
+# so that it only needs to be done once and runs continuously
+
+
 s = socket.socket()
 host = "0.0.0.0"
 port = 2000
@@ -7,8 +12,12 @@ port = 2000
 s.bind((host, port))
 s.listen(5)
 
+#print "Messages received from pi sent as [time, data] \n"
+
 while True:
 	c, addr = s.accept()
-	received = c.recv(1024)
-	print "Server received %s" %received
 
+	#receivedMessage = c.recv(1024)
+	#print "receivedMessage: " + receivedMessage
+	received = c.recv(1024)
+	print "Server received ", %received
